37f7dc66204f9c45cc285789b77eac01246f0431
Jacob Lee <68593007+jacobglee1@users.noreply.github.com>
2021-08-18 19:41:04 +0100

    Add files via upload

---
+++ mlp/TiO2.pp
@@ -0,0 +1,24 @@
+#units real # distances in A, energy in Kcal/mole, pressure in atmospheres,time in femtoseconds
+
+variable n2p2Cutoff equal  8.467 # largest symmetry function cutoff (Angstrom) (8.467A=16Bohr)
+variable n2p2Dir string "."
+
+mass 1 47.86 # Ti
+mass 2 16.00 # O
+
+set type 1 charge 1.6579436 # Ti
+set type 2  charge -0.82897182 # O
+
+kspace_style pppm 1e-5
+
+pair_style hybrid/overlay nnp dir ${n2p2Dir} showew no showewsum 10 resetew yes maxew 100000 cflength 1.8897261328 cfenergy 0.0015936014 emap "1:Ti,2:O" buck/coul/long  12.0
+
+# n2p2
+pair_coeff * * nnp ${n2p2Cutoff}
+
+# reMA
+pair_coeff 1 1 buck/coul/long 409063 0.154 68.9682 # Ti Ti
+pair_coeff 1 2 buck/coul/long 222900 0.194 165.524 # O Ti
+pair_coeff 2 2 buck/coul/long 154880 0.234 397.257 # O O
+
+
